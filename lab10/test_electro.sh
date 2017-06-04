85452feccac5c653eb2cf507c6de2ceecd44b200
Lorenz Gerber <lorenz.gerber@lablicate.com>
2017-06-04 14:32:01 +0200

    new report10

--- lab10/test_electro.sh
+++ lab10/test_electro.sh
@@ -8,8 +8,14 @@
 # Lorenz Gerber 30.05.17
 
 
+if [ -e "electrotest" ]; then
+    echo "Usage: give path to electrotest as argument"
+fi
+
+eval ./electrotest
 
 # Check for wrong resistnace values string, negative
+
 
 # Check extreme values
 
